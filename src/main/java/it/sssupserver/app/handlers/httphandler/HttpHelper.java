8cb4ae35f326b5672aa75b013aa2d6298aaa5179
Tredici <marcolucio1999@gmail.com>
2023-08-22 13:02:17 +0200

    Add HttpHelper.extractPostParametersOr400

--- src/main/java/it/sssupserver/app/handlers/httphandler/HttpHelper.java
+++ src/main/java/it/sssupserver/app/handlers/httphandler/HttpHelper.java
@@ -1,10 +1,13 @@
 package it.sssupserver.app.handlers.httphandler;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 import it.sssupserver.app.base.InvalidPathException;
 import it.sssupserver.app.base.Path;
 
+import com.sun.net.httpserver.HttpExchange;
 
 public class HttpHelper {
 
@@ -40,5 +43,43 @@ public class HttpHelper {
         var fixedPath = String.join("/", pathStack.toArray(new String[0]));
 
         return new Path(fixedPath, isDir);
+    }
+
+
+    public static Map<String, String> extractPostParametersOr400(HttpExchange exchange, String[] required) {
+        Map<String, String> ans = new HashMap<>();
+        // TODO - check encoding
+        var httpheader = exchange.getRequestHeaders();
+        if (!httpheader.containsKey("Content-Type")) {
+            var os = exchange.getResponseBody();
+            try {                
+                os.flush();
+                os.close();
+            } catch (Exception e) { }
+            return null;
+        }
+        var list = httpheader.get("Content-Type");
+        if (list.size() != 1) {
+            // Bad Request
+        }
+        var ContentType = list[0];
+        switch (ContentType) {
+            case "application/x-www-form-urlencoded":
+                break;
+            default:
+                break;
+        }
+        //Content-Type: application/x-www-form-urlencoded
+        // TODO - extract parameters from body
+
+        // assert existence of required parameters
+        var parameters = ans.keySet();
+        for (var r : required) {
+            if (!parameters.contains(r)) {
+                // 400!
+            }
+        }
+
+        return ans;
     }
 }
